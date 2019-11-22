676192b0101740965b375e9ff9bd88e827d8a8b9
SpongeBobSun <bobsun@outlook.com>
2019-11-22 22:40:49 +0800

    Stop removing build folder on build due to now it's my project root

--- build/build.js
+++ build/build.js
@@ -11,23 +11,22 @@ const config = require('../config/config');
 const spinner = ora("Building front end pages... \n\n");
 spinner.start();
 
+rm.sync(path.join(config.build.assetsRoot, 'css'))
+rm.sync(path.join(config.build.assetsRoot, 'js'))
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err;
-  webpack(webpackConfig, (err, stats) => {
-    spinner.stop();
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunk: false,
-      chunkModules: false
-    }) + '\n\n');
-    if (stats.hasErrors()){
-      console.log(chalk.red(' Build failed with errors. \n'));
-      process.exit(1);
-    }
-    console.log(chalk.cyan(' Build completed.\n'));
-  });
+webpack(webpackConfig, (err, stats) => {
+  spinner.stop();
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunk: false,
+    chunkModules: false
+  }) + '\n\n');
+  if (stats.hasErrors()){
+    console.log(chalk.red(' Build failed with errors. \n'));
+    process.exit(1);
+  }
+  console.log(chalk.cyan(' Build completed.\n'));
 });
 
